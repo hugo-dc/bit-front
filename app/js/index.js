5c82f51cc540db4bb7433e6ee221cf5ee94771b6
Jose Hugo De La Cruz Romero <hugo.delacruz@live.com.mx>
2015-09-08 12:08:51 -0500

    Create new notes, add functionality to Previous and Next buttons

--- app/js/index.js
+++ app/js/index.js
@@ -15,10 +15,14 @@ app.controller('MainController', function($scope) {
     $scope.markdown = "";
     $scope.nbook_ix = null;
     $scope.nbtitle = "";
+    $scope.action = null;
+    $scope.year = null;
+    $scope.month = null;
+    $scope.day = null;
 
   $scope.pgHome = function() {
     $scope.toggleVis('index');
-  }
+  };
 
   $scope.pgCreateNotebook = function() {
     $scope.toggleVis('create');
@@ -29,61 +33,77 @@ app.controller('MainController', function($scope) {
   };
 
 
-  $scope.toggleVis = function(name) {
-    $scope.current = name;
-    switch(name) {
-      case "index":
-        $scope.welcome_vis  = true;
-        $scope.createn_vis  = false;
-        $scope.loading_vis  = false;
-        $scope.search_vis   = false;
-        $scope.notebook_vis = false;
-	$scope.edit_vis     = false;
-        break;
-      case "create":
-        $scope.welcome_vis  = false;
-        $scope.createn_vis  = true;
-        $scope.loading_vis  = false;
-        $scope.search_vis   = false;
-        $scope.notebook_vis = false;
-	$scope.edit_vis     = false;
-        $scope.nb_name = "";
-        $scope.nb_desc = "";
-        break;
-      case "loading":
-        $scope.welcome_vis  = false;
-        $scope.createn_vis  = false;
-        $scope.loading_vis  = true;
-        $scope.search_vis   = false;
-        $scope.notebook_vis = false;
-	$scope.edit_vis     = false;
-        break;
-      case "search":
-        $scope.welcome_vis  = false;
-        $scope.createn_vis  = false;
-        $scope.loading_vis  = false;
-        $scope.search_vis   = true;
-        $scope.notebook_vis = false;
-	$scope.edit_vis     = false;
-        break;
-      case "notebook":
-        $scope.welcome_vis  = false;
-        $scope.createn_vis  = false;
-        $scope.loading_vis  = false;
-        $scope.search_vis   = false;
-        $scope.notebook_vis = true;
-	$scope.edit_vis     = false;
-        break;
-    case "edit":
-	$scope.welcome_vis  = false;
-        $scope.createn_vis  = false;
-        $scope.loading_vis  = false;
-        $scope.search_vis   = false;
-        $scope.notebook_vis = false;
-	$scope.edit_vis     = true;
-	break;
-    }
-  };
+    $scope.toggleVis = function(name) {
+	$scope.current = name;
+	// TODO: Fix this ugly code v
+	switch(name) {
+	case "index":
+            $scope.welcome_vis  = true;
+            $scope.createn_vis  = false;
+            $scope.loading_vis  = false;
+            $scope.search_vis   = false;
+            $scope.notebook_vis = false;
+	    $scope.edit_vis     = false;
+	    $scope.ntcreate_vis = false;
+            break;
+	case "create":
+            $scope.welcome_vis  = false;
+            $scope.createn_vis  = true;
+            $scope.loading_vis  = false;
+            $scope.search_vis   = false;
+            $scope.notebook_vis = false;
+	    $scope.edit_vis     = false;
+	    $scope.ntcreate_vis = false;
+            $scope.nb_name = "";
+            $scope.nb_desc = "";
+            break;
+	case "loading":
+            $scope.welcome_vis  = false;
+            $scope.createn_vis  = false;
+            $scope.loading_vis  = true;
+            $scope.search_vis   = false;
+            $scope.notebook_vis = false;
+	    $scope.edit_vis     = false;
+	    $scope.ntcreate_vis = false;
+            break;
+	case "search":
+            $scope.welcome_vis  = false;
+            $scope.createn_vis  = false;
+            $scope.loading_vis  = false;
+            $scope.search_vis   = true;
+            $scope.notebook_vis = false;
+	    $scope.edit_vis     = false;
+	    $scope.ntcreate_vis = false;
+            break;
+	case "notebook":
+            $scope.welcome_vis  = false;
+            $scope.createn_vis  = false;
+            $scope.loading_vis  = false;
+            $scope.search_vis   = false;
+            $scope.notebook_vis = true;
+	    $scope.edit_vis     = false;
+	    $scope.ntcreate_vis = false;
+            break;
+	case "edit":
+	    $scope.welcome_vis  = false;
+            $scope.createn_vis  = false;
+            $scope.loading_vis  = false;
+            $scope.search_vis   = false;
+            $scope.notebook_vis = false;
+	    $scope.edit_vis     = true;
+	    $scope.ntcreate_vis = false;
+	    break;
+	case "ntcreate":
+	    $scope.welcome_vis  = false;
+            $scope.createn_vis  = false;
+            $scope.loading_vis  = false;
+            $scope.search_vis   = false;
+            $scope.notebook_vis = false;
+	    $scope.edit_vis     = false;
+	    $scope.ntcreate_vis = true;
+	    break;
+	}
+    }; 
 
     $scope.createNotebook = function (name, desc) {
 	$scope.toggleVis("loading");
@@ -104,65 +124,119 @@ app.controller('MainController', function($scope) {
 	ipc.send('open-notebook', args);
     };
 
-  $scope.isActive = function(page) {
-    if (page == $scope.current)
-    {
-      return true; 
-    } else {
-      return false;
-    }
-  };
+    $scope.isActive = function(page) {
+	if (page == $scope.current)
+	{
+	    return true; 
+	} else {
+	    return false;
+	}
+    };
+    
 
     // Notes menu
+    $scope.callNote = function(nb,ix) {
+	console.log(nb);
+	console.log(ix);
+	var nb = $scope.notebooks[nb];
+	console.log(nb);
+	$scope.markdown = nb.notes[ix].content;
+	$scope.nbtitle  = nb.notes[ix].title;
+	$scope.year     = nb.notes[ix].year;
+	$scope.month    = nb.notes[ix].month;
+	$scope.day      = nb.notes[ix].day;
+	$scope.note_ix  = ix;
+	
+	document.getElementById('note').innerHTML = nb.notes[ix].html;
+    }
+    
+    $scope.mnPrev = function() {
+	if ($scope.note_ix > 0)
+	    $scope.callNote($scope.nbook_ix, $scope.note_ix - 1);	    
+    }
+
+    $scope.mnNext = function(){
+	if ($scope.note_ix < $scope.notebooks[$scope.nbook_ix].notes.length) 
+	    $scope.callNote($scope.nbook_ix, $scope.note_ix + 1);	    
+    }
+    
+    $scope.mnCreate = function() {
+	var curr = $scope.current;
+	$scope.tmptitle    = $scope.nbtitle;
+	$scope.tmpmarkdown = $scope.markdown;
+	
+	$scope.nbtitle = "";
+	$scope.markdown = "";
+	$scope.action = "create_note";	
+	$scope.toggleVis('edit');
+
+	$scope.current = curr;
+    };
+    
     $scope.mnEdit = function() {
 	var curr = $scope.current;
+	$scope.action = "update_note";
 	$scope.toggleVis('edit');
 	$scope.current = curr;
 	
-    }
+    };
 
+    // Thi function is used to CREATE or UPDATE a note
     $scope.mnViewHtml = function() {
-	console.log("mnViewHtml\n==========\n");
 	var curr = $scope.current;
+	console.log("mnViewHtml\n==========\n");
 	console.log("current: " + $scope.current);
+	console.log("action: " + $scope.action);
 	
-	var args = { "nbix"    : $scope.nbook_ix,
-		     "ntix"    : $scope.note_ix,
-		     "content" : null,
-		     "title"   : null
-		   };
-	var change = false;
-	
-	if ($scope.markdown != $scope.lastNote.content) {
-	    args.content = $scope.markdown;
-	    console.log("Markdown changed!");
-	    change = true;
-	}
-	if ($scope.nbtitle != $scope.lastNote.title) {
-	    args.title = $scope.nbtitle;
-	    console.log(args.nbtitle);
-	    console.log("Title changed!");
-	    change = true;
-	}
-	
-	if (change){
-	    $scope.toggleVis('loading');
-	    $scope.current = curr;
-	    console.log("Calling Update Backend proces...");
-	    console.log("Using args: nbix = " + $scope.nbook_ix + " note_ix = " + $scope.note_ix);
-	    ipc.send('update-note', args);
+	if ($scope.action == "create_note") {
+	    console.log("Create note...");
+	    var args = { "title" : $scope.nbtitle,
+			 "content" : $scope.markdown,
+			 "nbix" : $scope.nbook_ix,
+		       };
+	    console.log("Calling `Create Note` backend process...");
+	    ipc.send('create-note', args);
 	}else {
-	    console.log("Markdown not changed!");
-	    $scope.toggleVis('notebook');
-	    $scope.current = curr;
+	    console.log("Update note...");
+	    
+	    var args = { "nbix"    : $scope.nbook_ix,
+			 "ntix"    : $scope.note_ix,
+			 "content" : null,
+			 "title"   : null
+		       };
+	    var change = false;
+	
+	    if ($scope.markdown != $scope.lastNote.content) {
+		args.content = $scope.markdown;
+		console.log("Markdown changed!");
+		change = true;
+	    }
+	    if ($scope.nbtitle != $scope.lastNote.title) {
+		args.title = $scope.nbtitle;
+		console.log(args.nbtitle);
+		console.log("Title changed!");
+		change = true;
+	    }
+	
+	    if (change){
+		$scope.toggleVis('loading');
+		$scope.current = curr;
+		console.log("Calling Update Backend proces...");
+		console.log("Using args: nbix = " + $scope.nbook_ix + " note_ix = " + $scope.note_ix);
+		ipc.send('update-note', args);
+	    }else {
+		console.log("Markdown not changed!");
+		$scope.toggleVis('notebook');
+		$scope.current = curr;
+	    }
 	}
-    }
+    };
 });
 
 var getScope = function() {
-  var main = document.getElementById("html");
-  var sc = angular.element(main).scope();
-  return sc;
+    var main = document.getElementById("html");
+    var sc = angular.element(main).scope();
+    return sc;
 };
 
 ipc.on('notebook-exists', function() {
@@ -173,6 +247,7 @@ ipc.on('notebook-exists', function() {
     });
 });
 
+// This event is called when a new notebook is created
 ipc.on('notebook-ready', function(data) {
     var sc = getScope();
     var lastNB = data.nbs[data.nb_ix];
@@ -183,13 +258,16 @@ ipc.on('notebook-ready', function(data) {
 	sc.current = lastNB.id;
 	sc.markdown = lastNB.notes[data.nt_ix].content;
 	sc.nbtitle  = lastNB.notes[data.nt_ix].title;
-	sc.lastNote = lastNB.notes[data.nt_ix];
+	sc.year     = lastNB.notes[data.nt_ix].year;
+	sc.month    = lastNB.notes[data.nt_ix].month;
+	sc.day      = lastNB.notes[data.nt_ix].day;
+	sc.lastNote = lastNB.notes[data.nt_ix]; // TODO: Fix
 	sc.nbook_ix = data.nb_ix;
 	sc.note_ix  = data.nt_ix;
- });
+    });
 });
 
-ipc.on('loaded-notebooks', function (data) {
+    ipc.on('loaded-notebooks', function (data) {
   var sc = getScope();
   sc.$apply(function() {
     sc.notebooks = data;
--- main.js
+++ main.js
@@ -82,22 +82,55 @@ var getDMY = function() {
 
     return [d,m,y];
 }
-var getHtml = function(markdown) {
+var getHtml = function(key, markdown) {
     var result = null;
     var fs = require('fs');
-    var dmy = getDMY();
     var child_process = require('child_process');
 
-    d = dmy[0];
-    m = dmy[1];
-    y = dmy[2];
-    var fname = y + '-' + m + '-' + d + '-convert';
+    d = getDay(key);
+    m = getMonth(key);
+    y = getYear(key);
+    
+    var fname = y + '-' + m + '-' + d + '-' + key;
     
     fs.writeFileSync('./bin/posts/' + fname + '.md', markdown);
     var ch = child_process.spawnSync(__dirname + '\\bin\\build.bat', [], {cwd: __dirname + '\\bin'});
     var html = fs.readFileSync('./bin/_site/posts/' + fname + '.html');
-    console.log(html.toString());
+
     return html.toString();
+}
+
+var generateKey = function() {
+    var today = new Date();
+    var ms    = today.getMilliseconds().toString();
+    var s     = today.getSeconds().toString();
+    var mn    = today.getMinutes().toString();
+    var h     = today.getHours().toString();
+    var d     = today.getDate().toString();
+    var m     = (today.getMonth() + 1 ).toString();
+    var y     = today.getFullYear().toString();
+
+    if (m < 10) {
+	m = "0" + m;
+    }
+    if (d < 10) {
+	d = "0" + d;
+    }
+    
+    var key = y + m + d + h + mn + s + ms;
+    return key;
+}
+
+var getYear = function(key) {
+    return key.substring(0,4);
+}
+
+var getMonth = function(key) {
+    return key.substring(4,6);
+}
+
+var getDay = function(key) {
+    return key.substring(6,8);
 }
 
 // Report crashes to our server.
@@ -140,26 +173,27 @@ app.on('ready', function() {
 
 // Create Notebook
 ipc.on('create-notebook', function(event, args) {
-    var dmy = getDMY();
-    var d = dmy[0];
-    var m = dmy[1];
-    var y = dmy[2];
+    var key = generateKey();
+    var d   = getDay(key);
+    var m   = getMonth(key);
+    var y   = getYear(key);
     var defmd = "Welcome!\n========\n\n\nHi!, Welcome to BeeLog, your personal notebook!.\n\nYou write your personal/job blog using BeeLog and Markdown Syntax!. Here is a simple explanation of the Markdown Syntax:\n\nMarkdown Syntax\n===============\n\nMarkdown Syntax is a way to write text that is readible for people but that it\ncan also be converted to HTML.\n\nLet's go through a series of examples on how to write Markdown Syntax.\n\n\nExamples:\n\n------------------\n\nThe following markdown text:\n\n```markdown\nYou can write Headers\n=====================\n```\n\n\nProduces the following HTML:\n\nYou can write Headers\n=====================\n\n\n------------------\n\nMarkdown text:\n\n```markdown\nYou can write subheaders\n------------------------\n```\n\nHTML:\n\nYou can write subheaders\n------------------------\n\n------------------\n\nMarkdown:\n\n```markdown\n**You can write bold text**\n```\n\nHTML:\n\n**You can write bold text**\n\n------------------\n\nMarkdown:\n\n```markdown\n*You can write text in italics*\n```\n\n\nHTML:\n\n*You can write text in italics*\n\n------------------\n\nMarkdown:\n\n```markdown\n1. Item 1 \n1. Item 2\n1. Item 3\n```\n\nHTML:\n\n\n1. Item 1 \n1. Item 2\n1. Item 3\n\n-------------------\n\nMarkdown:\n\n```markdown\n* Item 1\n* Item 2\n* Item 3\n```\n\nHTML: \n\n* Item 1\n* Item 2\n* Item 3\n\n--------------------\n\nMarkdown:\n\n```markdown\n[This is a link to Google](http://www.google.com/)\n```\n\nHTML:\n\n[This is a link to Google](http://www.google.com/)\n\n\n------------------\n\nMarkdown:\n\n```markdown\n![image](http://fc00.deviantart.net/fs71/f/2011/129/6/c/dptux_by_teliok-d3fyij5.png)\n```\n\nHTML:\n\n![image](http://fc00.deviantart.net/fs71/f/2011/129/6/c/dptux_by_teliok-d3fyij5.png)\n\n-----------------\n\nMarkdown:\n\n```markdown\n\n ```abap\nREPORT znumbers.\n\ndata:\nmy_int type i.\n\nparameters:\nmy_hex type x.\n\nmy_int = my_hex.\n\nwrite:/ 'HEX: ', my_hex, /'INT: ', my_int.\n ```\n\n```\n\nHTML:\n\n```abap\nREPORT znumbers.\n\ndata:\nmy_int type i.\n\nparameters:\nmy_hex type x.\n\nmy_int = my_hex.\n\nwrite:/ 'HEX: ', my_hex, /'INT: ', my_int.\n```\n\n-----------------\n\nMarkdown\n\n```markdown\n ```javascript\n var a = 0;\n var b = 1;\n\n var c = a + b;\n\n alert(c);\n\n\n ```\n```\n\n\n```javascript\nvar a = 0;\nvar b = 1;\n\nvar c = a + b;\n\nalert(c);\n```\n";
-    var defhtml = getHtml(defmd);
+    var defhtml = getHtml(key, defmd);
 
-    var key = y + '-' + m + '-' + d + '-default';
-  
-  ndata.id    = args.name.replace(/\s+/g, '').toLowerCase(); 
-  ndata.icon  = 'fa-edit';
-  ndata.name  = args.name;
-  ndata.desc  = args.desc;
-  ndata.notes = [{key: key, 
-                  title: "Your First Post",
-                  content: defmd, 
-                  html:    defhtml, 
-                  prev: null,
-                  next: null
-                 }];
+    ndata.id    = args.name.replace(/\s+/g, '').toLowerCase(); 
+    ndata.icon  = 'fa-edit';
+    ndata.name  = args.name;
+    ndata.desc  = args.desc;
+    ndata.notes = [
+	{key    : key,
+	 year   : y,
+	 month  : m,
+	 day    : d,
+         title  : "Your First Post",
+         content: defmd, 
+         html:    defhtml, 
+        }
+    ];
   //ndata.link  = "#";
   ndata.click = 'openNotebook(' + args.name + ')';
 
@@ -192,8 +226,52 @@ ipc.on('open-notebook', function(event, args) {
     var ix = getNotebookIndex(args.id);
     var nbs = getNotebooks();
     var ntix = nbs[ix].notes.length - 1;
+    console.log(ntix);
 
-    mainWindow.webContents.send('notebook-ready', {"nbs": nbs, "nb_ix": ix, "nt_ix": ntix });
+    mainWindow.webContents.send('notebook-ready',
+				{
+				    "nbs": nbs,
+				    "nb_ix": ix,
+				    "nt_ix": ntix
+				});
+});
+
+// Create note
+ipc.on('create-note', function(event, args) {
+    console.log("Entering `create-note` backend process");
+
+    var nb  = getNotebookByIx(args.nbix); 
+    var key = generateKey();
+    var d   = getDay(key);
+    var m   = getMonth(key);
+    var y   = getYear(key);
+    
+    var html = getHtml(key, args.content);
+    
+    var note = {key    : key,
+		year   : y,
+		month  : m,
+		day    : d,
+		title  : args.title,
+		content: args.content,
+		html:    html
+	       };
+
+    nb.notes.push(note);
+    var ntix = nb.notes.length - 1;
+    
+    var ix = settings.length - 1;
+    settings = db.settings.find();
+    console.log("Saving in database...");
+    settings[ix]["notebooks"][args.nbix] = nb;
+    db.settings.save(settings[ix]);
+
+    mainWindow.webContents.send('notebook-ready',
+				{"nbs" : settings[ix].notebooks,
+				 "nb_ix" : args.nbix,
+				 "nt_ix" : ntix
+				}
+			       );
 });
 
 // Update note
@@ -207,7 +285,7 @@ ipc.on('update-note', function(event, args) {
     var ix = settings.length - 1;
 
     if (args.content) {
-	var html = getHtml(args.content);
+	var html = getHtml(note.key, args.content);
 	console.log("Updating note content:");
 	console.log(JSON.stringify(nb.notes[args.ntix]));
 	nb.notes[args.ntix].content = args.content;
