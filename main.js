95e2382eecb624709409f53f6fcdcfb57ef3f75c
Jesus <chise1986@bitbucket.org>
2015-09-28 12:19:09 -0600

    open in fullscreen mode directly

--- main.js
+++ main.js
@@ -153,7 +153,7 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ width: 800, height: 600, fullscreen: true });
   mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 
   // and load the index.html of the app.
